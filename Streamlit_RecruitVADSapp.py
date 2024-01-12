458c6a3e97f4801f9e97d191ede2024d2122fa26
Divya-coder-isb <156130108+Divya-coder-isb@users.noreply.github.com>
2024-01-12 22:19:43 +0530

    Add files via upload

--- Streamlit_RecruitVADSapp.py
+++ Streamlit_RecruitVADSapp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 # Import the required libraries
@@ -9,7 +9,6 @@ import streamlit as st
 import pandas as pd
 import pickle
 import requests
-from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
 # Define a function to load the data and the model
@@ -43,10 +42,8 @@ def get_relevancy_score(row):
     candidate_text = " ".join([job_title, skills, experience, certification])
     # Vectorize the candidate's text using the Modified Resume Data
     candidate_vector = vectorizer.transform([candidate_text])
-    # Vectorize the user's input
-    input_vector = vectorizer.transform([" ".join([role, skills, experience, certification])])
     # Use the trained model to predict the relevancy score
-    relevancy_score = model.predict(cosine_similarity(candidate_vector, input_vector))[0]
+    relevancy_score = model.predict(candidate_vector)[0]
     # Clip the score to the range [0, 100]
     relevancy_score = np.clip(relevancy_score, 0, 100)
     # Return the score
