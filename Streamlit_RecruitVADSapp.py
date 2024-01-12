82a0a10220351a94512d55594eb155d8bbabbd93
Divya-coder-isb <156130108+Divya-coder-isb@users.noreply.github.com>
2024-01-12 14:18:54 +0530

    Add files via upload

--- Streamlit_RecruitVADSapp.py
+++ Streamlit_RecruitVADSapp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[43]:
+# In[44]:
 
 
 # Import the required libraries
@@ -40,7 +40,7 @@ def get_relevancy_score(row):
     return score
 
 # Display the image on top of the page with increased width
-image_width = 300  
+image_width = 150  
 st.image(image_url, use_column_width=image_width)
 
 # Create a two-column layout for the input and output fields
